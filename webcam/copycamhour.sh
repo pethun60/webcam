2ede4f75f0338a8375b1ef7f5a23e8a03646e004
pethun60 <pethun@gmail.com>
2018-02-23 11:19:00 +0100

    test

--- webcam/copycamhour.sh
+++ webcam/copycamhour.sh
@@ -1,12 +1,11 @@
 #/bin/sh
-cp -f /home/peter/mount/new_webserver/Pictures/cam9.jpg /home/peter/mount/new_webserver/Pictures/cam10.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam8.jpg /home/peter/mount/new_webserver/Pictures/cam9.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam7.jpg /home/peter/mount/new_webserver/Pictures/cam8.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam6.jpg /home/peter/mount/new_webserver/Pictures/cam7.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam5.jpg /home/peter/mount/new_webserver/Pictures/cam6.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam4.jpg /home/peter/mount/new_webserver/Pictures/cam5.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam3.jpg /home/peter/mount/new_webserver/Pictures/cam4.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam2.jpg /home/peter/mount/new_webserver/Pictures/cam3.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam1.jpg /home/peter/mount/new_webserver/Pictures/cam2.jpg
-cp -f /home/peter/mount/new_webserver/Pictures/cam.jpg /home/peter/mount/new_webserver/Pictures/cam1.jpg
-test
+cp -f /home/peter/mount/new_webserver/pictures/cam9.jpg /home/peter/mount/new_webserver/Pictures/cam10.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam8.jpg /home/peter/mount/new_webserver/Pictures/cam9.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam7.jpg /home/peter/mount/new_webserver/Pictures/cam8.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam6.jpg /home/peter/mount/new_webserver/Pictures/cam7.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam5.jpg /home/peter/mount/new_webserver/Pictures/cam6.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam4.jpg /home/peter/mount/new_webserver/Pictures/cam5.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam3.jpg /home/peter/mount/new_webserver/Pictures/cam4.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam2.jpg /home/peter/mount/new_webserver/Pictures/cam3.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam1.jpg /home/peter/mount/new_webserver/Pictures/cam2.jpg
+cp -f /home/peter/mount/new_webserver/pictures/cam.jpg /home/peter/mount/new_webserver/Pictures/cam1.jpg
