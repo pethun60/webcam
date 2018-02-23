624b1247a589c7f464bf26623314dde28db6ddb9
pethun60 <pethun@gmail.com>
2018-02-23 11:38:51 +0100

    latest

---
+++ webcam/picture_created.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp -f /home/peter/motion/pictures/*snapshot.jpg /home/peter/mount/new_webserver/Pictures/cam.jpg
+# cp /home/peter/motion/pictures/*snapshot.jpg /home/peter/motion/pictures/picturehistoryfiles
+rm -f /home/peter/motion/pictures/*snapshot.jpg
